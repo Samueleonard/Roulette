8c0ff0810f4dfd10f35c6f46e1219dfca7fd223e
samster03 <34142670+samster03@users.noreply.github.com>
2018-09-15 00:06:20 +0100

    Add files via upload

---
+++ roulette.py
@@ -0,0 +1,33 @@
+import random
+colours = ["green", "red", "blue"]
+dozen = ["1st","2nd","3rd"]
+choice = ["number", "dozen", "colour", "other"]
+
+total_money = 10000
+
+result_colour = random.choice(colours)
+result_number = random.randint(0, 36)
+
+def __init__():
+    place_bet()
+
+def place_bet():
+    bet = int(input("how much money do you want to bet"))
+    if bet > total_money:
+        print("invalid")
+        while bet > total_money:
+            bet = int(input("how much money do you want to bet"))
+    choice = input("what choice do you want to make (choose from: number, dozen, colour or other)")
+    choice = choice.lower()
+    get_result(choice)
+
+def get_result(choice):
+    if choice == "number":
+        print(result_number)
+    elif choice == "colour":
+        print(result_colour)
+    else:
+        print("test")
+    __init__()
+
+__init__()
