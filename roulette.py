509a60912dc56788c3c41e728a2e90faa76f6ac5
rnelson2002 <43282068+rnelson2002@users.noreply.github.com>
2018-09-15 18:27:01 +0100

    money fixes

--- roulette.py
+++ roulette.py
@@ -4,35 +4,47 @@ dozen = ["1st","2nd","3rd"]
 choice = ["number", "dozen", "colour", "other"]
 
 total_money = 10000
+bet = 0
 
-result_colour = random.choice(colours)
-result_number = random.randint(0, 36)
 
-def __init__():
-    place_bet()
 
-def place_bet():
+
+def __init__(total_money):
+    while 0 == 0:
+        global result_number
+        global result_colour
+        result_colour = random.choice(colours)
+        result_number = random.randint(0, 36)
+        total_money = place_bet(total_money)
+
+def place_bet(new_money):
     bet = int(input("how much money do you want to bet"))
-    if bet > total_money:
+    if bet > new_money:
         print("invalid")
-        while bet > total_money:
+        while bet > new_money:
             bet = int(input("how much money do you want to bet"))
     choice = input("what choice do you want to make (choose from: number, dozen, colour or other)")
     choice = choice.lower()
-    get_result(choice)
+    new_money = get_result(choice, new_money, bet)
+    return new_money
 
-def get_result(choice):
+def get_result(choice, totalMoney, bet):
     if choice == "number":
         print(result_number)
         input_number = int(input("choose a number"))
         if input_number == result_number:
             print("you won")
+            total_money = totalMoney + bet
+            print("your total money is", total_money)
+            return total_money
         else:
             print("you lost, unlucky")
+            total_money = totalMoney - bet
+            print("your total money is:", total_money)
     elif choice == "colour":
         print(result_colour)
     else:
         print("test")
     __init__()
 
-__init__()
+__init__(total_money)
