62c2b7f8cbda1f3fb7e0b5ea7a622762cb2f1ee1
samster03 <34142670+samster03@users.noreply.github.com>
2018-09-20 18:33:29 +0100

    v 0.15 - added ui.py script

---
+++ ui.py
@@ -0,0 +1,44 @@
+from tkinter import *
+root = Tk()
+Button(text="0",bg="green",fg="white",padx=10, pady=54, command=lambda: guessnum(int(r))).grid(rowspan=3, row=1, column=1)
+r = 0
+root.configure(bg="#09681a")
+for c in range(4):
+    r = r + 3
+    Button(root,text=str(r), bg="red",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=1,column=int((r/3)+1))
+    r = r + 3
+    Button(root,text=str(r), bg="black",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=1,column=int((r/3)+1))
+    r = r + 3
+    Button(root,text=str(r), bg="red",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=1,column=int((r/3)+1))
+r = -1
+for fghfdhd in range(4):
+    r = r + 3
+    Button(root,text=str(r), bg="black",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=2,column=int((r+1)/3)+1)
+    r = r + 3
+    Button(root,text=str(r), bg="red",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=2,column=int((r+1)/3)+1)
+    r = r + 3
+    Button(root,text=str(r),highlightbackground="white", bg="black",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=2,column=int((r+1)/3)+1)
+
+r = -2
+for fghfdhd in range(2):
+    r = r + 3
+    Button(root,text=str(r), bg="red",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=3,column=int((r+2)/3)+1)
+    r = r + 3
+    Button(root,text=str(r),bg="black",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=3,column=int((r+2)/3)+1)
+    r = r + 3
+    Button(root,text=str(r), bg="red",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=3,column=int((r+2)/3)+1)
+    r = r + 3
+    Button(root,text=str(r), bg="black",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=3,column=int((r+2)/3)+1)
+    r = r + 3
+    Button(root,text=str(r), bg="black",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=3,column=int((r+2)/3)+1)
+    r = r + 3
+    Button(root,text=str(r), bg="red",fg="white", padx=10, pady=10, command=lambda: guessnum(int(r))).grid(row=3,column=int((r+2)/3)+1)
+canvas = Canvas(root,width=400,height=400,highlightthickness=0,bd=0,bg="#09681a")
+canvas.grid(row=40,column=40)
+canvas.create_oval(0, 0, 400, 400, fill = "black")
+canvas.create_text(200,200, text="hi",fill="white",font=("Helvetica", 100))
+
+mainloop()
+
+
+
